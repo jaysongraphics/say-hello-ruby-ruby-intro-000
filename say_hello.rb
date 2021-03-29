01cdb39dc70148e6df8810f4423f812e8dbc22ef
pleasant-tree-5cd4 <jaysongraphics@gmail.com>
2021-03-29 14:56:24 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
-# Build your say_hello method here
-def say_hello(name = "Ruby Programmer!")
-  puts "Hello #{name}"
+def say_hello (name = "Ruby Programming")
+  puts "Hello #{name}!"
 end
 
-say_hello ("Hello! ")
+say_hello ("Gabriela")
